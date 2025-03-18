a9e6af997908a12595586e8f6768b6fd3f4f93a5
Aaron Ochieng <ochiengaoron@outlook.com>
2025-03-18 10:24:45 +0300

    Add: login template - authentication form login string

---
+++ js/auth/ui.js
@@ -0,0 +1,18 @@
+export const auth_ui = `
+    <div class="login-container">
+        <div class="login-header">
+            <h2>Login</h2>
+        </div>
+        <div id="login-form">
+            <div class="input-group">
+                <label for="username">Username /  Email</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button id="submit">Login</button>
+        </div>
+    </div>
+`
