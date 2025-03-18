a284c9dd6500f03b19ac6621041592b9a671a724
Aaron Ochieng <ochiengaoron@outlook.com>
2025-03-18 10:25:55 +0300

    add: content ui string

--- js/auth/ui.js
+++ js/auth/ui.js
@@ -16,3 +16,30 @@ export const auth_ui = `
         </div>
     </div>
 `
+
+export const content_ui = `
+    <div class="grid-container">
+        <div class="grid-item">
+            <div class="icon">
+                A
+            </div>
+            <div class="user-details">
+                <p class="username">Aaron Ochieng</p>
+                <p class="username">+254740707743</p>
+                <p class="username">ochiengaoron@gmail.com</p>
+            </div>
+        </div>
+        <div class="grid-item">
+            <div class="icon">
+                A
+            </div>
+            <div class="user-details">
+                <p class="username">Aaron Ochieng</p>
+                <p class="username">+254740707743</p>
+                <p class="username">ochiengaoron@gmail.com</p>
+            </div>
+        </div>
+        <div class="grid-item">Item 2</div>
+        <div class="grid-item">Item 3</div>
+    </div>
+`
